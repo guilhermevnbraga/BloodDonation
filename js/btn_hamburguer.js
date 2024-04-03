5e44c112682f9d889979a8be6e130f052b504494
Guilherme Braga <crazyboysfh3@gmail.com>
2024-04-03 16:28:34 -0300

    melhorado css hamburguer

--- js/btn_hamburguer.js
+++ js/btn_hamburguer.js
@@ -1,10 +1,13 @@
 const btnHamburguer = document.getElementsByClassName('header__hamburguer')[0];
 const menu = document.getElementsByClassName('header-nav')[0];
+const menu2 = document.getElementsByClassName('perfil-nav')[0];
 const header = document.querySelector('.main__header')
 console.log(header)
 
 const outMenu = ()=>{
         menu.classList.toggle('is-none');
+        menu2.classList.toggle('is-none');
+        menu2.classList.toggle('col');
         header.classList.toggle('modal-height')
 }
 
